c84a83bc64364576033fbf6b57c64f27707b7e06
NormantasKudzma <kudzma.normantas@gmail.com>
2014-09-27 14:37:29 +0300

    Added stub classes

---
+++ src/api/PaintBase.java
@@ -0,0 +1,11 @@
+package api;
+
+import java.awt.Graphics2D;
+
+public class PaintBase {
+	protected Graphics2D g;
+	
+	public PaintBase(Graphics2D graphics){
+		this.g = graphics;
+	}
+}
---
+++ src/api/PaintGUI.java
@@ -0,0 +1,10 @@
+package api;
+
+import java.awt.Graphics2D;
+
+public class PaintGUI extends PaintBase {
+	
+	public PaintGUI(Graphics2D graphics){
+		super(graphics);
+	}
+}
