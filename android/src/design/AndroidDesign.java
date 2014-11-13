1d871a3a0de4d902885c32c880c583a67543db87
NormantasKudzma <kudzma.normantas@gmail.com>
2014-11-13 11:59:59 +0200

    Fixed (some) bugs in serverside

--- android/src/design/AndroidDesign.java
+++ android/src/design/AndroidDesign.java
@@ -1,8 +1,56 @@
 package design;
 
+import java.io.ByteArrayOutputStream;
+
+import javax.imageio.ImageIO;
+
 public class AndroidDesign extends PcDesign{
 	public AndroidDesign() {
 		super();
-		new CommServer().start();
+		(new Thread(new CommServer(this))).start();
+	}
+	
+	public synchronized void processAction(String str){
+		System.out.println("Draw called " + str);
+		String [] arr = str.split(" ");
+		int [] coords = new int[arr.length];
+		for (int i = 0; i < arr.length; i++){
+			coords[i] = Integer.parseInt(arr[i]);
+		}
+		if (coords.length > 2){
+			dispatchMouseDrag(coords);
+		}
+		else {
+			dispatchMouseClick(coords);
+		}
+	}
+	
+	public void dispatchMouseDrag(int [] arr){
+		paint.drawCenteredLine(arr[0], arr[1], arr[2], arr[3]);
+		drawContainerPanel.repaint();
+	}
+	
+	public void dispatchMouseClick(int [] arr){
+		paint.drawCenteredPixel(arr[0], arr[1]);
+		drawContainerPanel.repaint();
+	}
+	
+	public synchronized String getImageBytes(){
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(drawing, "jpg", baos);
+			baos.flush();
+			String arr = new String(baos.toByteArray());
+			baos.close();
+			return arr;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void main(String [] args){
+		new AndroidDesign();
 	}
 }
--- android/src/design/CommServer.java
+++ android/src/design/CommServer.java
@@ -1,17 +1,71 @@
 package design;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class CommServer extends Thread{
+	public static final int PORT = 60210;
+	
+	Socket socket;
+	BufferedReader reader;
+	PrintWriter writer;
+	AndroidDesign design;
+	
 	// wait for connection, start thread
 	boolean connected = false;
-	Socket socket;
+	boolean done = false;
+	
+	public CommServer(AndroidDesign design){
+		this.design = design;
+	}
 	
 	@Override
 	public void run() {
-		while (!connected){
-			ServerSocket s;
+		try {
+			ServerSocket server = new ServerSocket(PORT);					
+			server.setReuseAddress(true);
+			System.out.println("Socket is now accepting connections");
+			socket = server.accept();				
+			System.out.println("Connected " + socket.getInetAddress());
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			writer = new PrintWriter(socket.getOutputStream());
+			server.close();
+			
+			while (!done && checkConnection()){
+				String command = reader.readLine();
+				System.out.println("Got command from phone " + command);
+				design.processAction(command);
+				writer.println(design.getImageBytes());
+				writer.flush();
+			}
+			stopClient();
 		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void stopClient(){
+		try {
+			System.out.println("Server stopped");
+			done = true;
+			if (writer != null) writer.close();
+			if (reader != null) reader.close();
+			if (socket != null && !socket.isClosed()) socket.close();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private boolean checkConnection(){
+		boolean isConnected = socket.isConnected();
+		if (!isConnected){
+			stopClient();
+		}
+		return isConnected;
 	}
 }
