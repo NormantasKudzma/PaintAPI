36aac8eca3504319f945186c8bb534fc3a15f174
NormantasKudzma <kudzma.normantas@gmail.com>
2014-11-25 19:15:45 +0200

    LeapMotion now works with 2 users simultaneously painting, added HSwipe gesture detection to change colors (works on both hands)

--- leap/src/design/LeapController.java
+++ leap/src/design/LeapController.java
@@ -5,27 +5,39 @@ import com.leapmotion.leap.Finger;
 import com.leapmotion.leap.Finger.Type;
 import com.leapmotion.leap.FingerList;
 import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Gesture;
+import com.leapmotion.leap.GestureList;
 import com.leapmotion.leap.Hand;
 import com.leapmotion.leap.HandList;
 import com.leapmotion.leap.Listener;
+import com.leapmotion.leap.SwipeGesture;
 import com.leapmotion.leap.Vector;
 
-public class LeapController extends Listener {
+public class LeapController extends Listener implements Runnable{
 	public static final double DRAW_TRESHOLD = 70;
-	public static int X_SENSITIVITY = 250;	// X sensitivity (inverted)
-	public static int Y_SENSITIVITY = 280;	// Y sensitivity (inverted)
+	public static final int X_SENSITIVITY = 250;	// X sensitivity (inverted)
+	public static final int Y_SENSITIVITY = 280;	// Y sensitivity (inverted)
 	
-	public static int Y_DELTA_CORRECTION = -300;
-	public static int X_DELTA_CORRECTION = 800;
+	// Right hand correction , left hand correction
+	public static final int Y_DELTA_CORRECTION = -300;
+	public static final int [] X_DELTA_CORRECTION = new int[]{300, 1200};
 	
 	double [][][] trend = new double[2][7][2]; //index x y
-	
 	boolean isDrawing [] = new boolean[]{false, false};
+	long sleepInterval = 33;
+	long gestureInterval = 100;
+	long lastGesture [] = new long[2];
 	
 	LeapDesign d;
+	Controller c;
+	boolean done = false;;
 	
 	public LeapController(LeapDesign d){
 		this.d = d;
+	}
+	
+	public void setController(Controller c){
+		this.c = c;
 	}
 	
 	@Override
@@ -48,13 +60,48 @@ public class LeapController extends Listener {
 		System.out.println("Leapmotion exited");
 	}
 	
+	public boolean isDone(){
+		return done;
+	}
+	
+	public void isDone(boolean done){
+		this.done = done;
+	}
+	
 	@Override
-	public void onFrame(Controller c) {
+	public void run() {
+		while (!done){
+			try {
+				Thread.sleep(sleepInterval);
+				performActions(c);
+			}
+			catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	void performActions(Controller c){
 		Frame f = c.frame();
+		// Set image for videoPanel
+//		d.setImage(f.images().get(0));
+		
+		// Grab gestures and hand motions, process them
+		Gesture g = f.gestures().get(0);
+		Hand gestureHand = g != null ? g.hands().get(0) : null;
 		HandList hands = f.hands();
-		for (int i = 0; i < hands.count(); i++){
-			Hand hand = hands.get(i);
-			int index = i > 0 ? 1 : 0;
+		for (Hand hand : hands){
+			int index = hand.isLeft() ? 1 : 0;
+			if (gestureHand != null && gestureHand.id() == hand.id() && g.type() == Gesture.Type.TYPE_SWIPE){
+				long time = System.currentTimeMillis();
+				if (lastGesture[index] < time){
+					SwipeGesture sg = new SwipeGesture(g);
+					float x = sg.direction().getX();
+					int dir = x > 0 ? 1 : -1;
+					d.nextColor(index, dir);
+				}
+				lastGesture[index] = time + gestureInterval;
+			}
 			int closedFingers = 0;
 			Vector hPos = hand.palmPosition();
 			FingerList fingers = hand.fingers();
@@ -74,15 +121,14 @@ public class LeapController extends Listener {
 
 			if (drawFinger != null){
 				double [] pos = new double[]{drawFinger.tipPosition().getX(), drawFinger.tipPosition().getY()};
+				pos = Smoothing.doubleAverageSmoothing(pos, trend[index]);
 				trend[index] = matrixPush(trend[index]);
 				trend[index][0][0] = pos[0];
 				trend[index][0][1] = pos[1];
-				pos = Smoothing.doubleAverageSmoothing(pos, trend[index]);
 				pos = Smoothing.exponentialSmoothing(pos, trend[index]);
 				
-				d.thisX[index] = convertX(pos[0]);
+				d.thisX[index] = convertX(index, pos[0]);
 				d.thisY[index] = convertY(pos[1]);
-				System.out.println("new XY " + d.thisX[index] + "\t" + d.thisY[index]);
 				d.glass.repaint();
 				
 				if (closedFingers > 2){
@@ -104,6 +150,10 @@ public class LeapController extends Listener {
 		}
 	}
 	
+	@Override
+	public void onFrame(Controller c) {
+	}
+	
 	private double[][] matrixPush(double [][] m){
 		for (int i = m.length - 1; i > 0; i--){
 			m[i] = m[i-1];
@@ -111,11 +161,11 @@ public class LeapController extends Listener {
 		return m;
 	}
 	
-	public int convertX(double x){
+	public int convertX(int index, double x){
 		int newX = 0;
 		int fw = KinectDesign.frameWidth;
 		double coef = 1.0 * fw / X_SENSITIVITY;	
-		newX = (int) (coef * x + X_DELTA_CORRECTION);
+		newX = (int) (coef * x + X_DELTA_CORRECTION[index]);
 		newX = Math.max(0, Math.min(fw, newX));
 		return newX;
 	}
--- leap/src/design/LeapDesign.java
+++ leap/src/design/LeapDesign.java
@@ -1,20 +1,31 @@
 package design;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.JColorChooser;
+import javax.swing.JPanel;
 
 import com.leapmotion.leap.Controller;
-
-import core.PaintBase;
+import com.leapmotion.leap.Gesture;
 
 public class LeapDesign extends KinectDesign{
 	LeapController leapMotion;
 	Controller c;
 	
+	KinectColorChooser kcc;
+	Image lastImage;
+	JPanel leapVideoPanel;
+	
 	public LeapDesign(){
-		/////////////////////////////////////////
-		// DEBUG 
-		//new Thread(new MultiUserTest()).start();
-		/////////////////////////////////////////
+		setTitle("The amazing paint for LeapMotion, v1.1");
+		initColorChooser();
+		initVideoPanel();
 	}
 	
 	@Override
@@ -22,7 +33,65 @@ public class LeapDesign extends KinectDesign{
 		System.out.println("Setup device called");
 		leapMotion = new LeapController(this);
 		c = new Controller(leapMotion);
+		c.enableGesture(Gesture.Type.TYPE_SWIPE);
+		c.setPolicy(Controller.PolicyFlag.POLICY_IMAGES);
+		leapMotion.setController(c);
+			
+		new Thread(leapMotion).start();
 	}
+	
+	void initVideoPanel(){
+		videoPanel.stopAnimation();
+		rightPanel.remove(0);
+		leapVideoPanel = new JPanel(){
+			protected void paintComponent(java.awt.Graphics g) {
+				if (lastImage != null) {
+					g.drawImage(lastImage, 0, 0, null);
+				}
+			};
+		};
+		rightPanel.add(leapVideoPanel, 0);
+		rightPanel.invalidate();
+	}
+	
+	void initColorChooser(){
+		JColorChooser jcc = (JColorChooser)((JPanel)topPanel.getComponents()[0]).getComponent(0);
+		kcc = (KinectColorChooser) jcc.getChooserPanels()[0];
+	}
+	
+	void nextColor(int index, int direction){
+		Color clr = kcc.getSelectedColors()[index];
+		int clrNum = (getColorNum(clr) + direction) % kcc.all.length;
+		if (clrNum < 0){
+			clrNum = kcc.all.length - 1;
+		}
+		ActionEvent evt = new ActionEvent(kcc.all[clrNum], index, null);
+		kcc.actionPerformed(evt);
+	}
+	
+	int getColorNum(Color c){
+		for (int i = 0; i < kcc.all.length; i++){
+			if (kcc.all[i].getBackground() == c){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+//	void setImage(com.leapmotion.leap.Image img){
+//		try {
+//			if (leapVideoPanel != null){
+//				byte [] arr = img.data();
+//				InputStream in = new ByteArrayInputStream(arr);
+//				BufferedImage bi = ImageIO.read(in);
+//				lastImage = bi;
+//				leapVideoPanel.repaint();
+//			}
+//		}
+//		catch (Exception e){
+//			e.printStackTrace();
+//		}
+//	}
 	
 	public static void main(String [] args){
 		new LeapDesign();
